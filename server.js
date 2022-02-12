b305a5d3a81ee8ce40784ea450b40244416678ef
Robert Albenda <ralbenda@gmail.com>
2022-02-11 21:24:43 -0500

    added linkages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize("postgres://localhost/acme_bookmarks");
 const express = require("express");
 const app = express();
 
-const bookmark = sequelize.define("bookmark", {
+const Bookmark = sequelize.define("bookmark", {
   website: {
     type: Sequelize.DataTypes.STRING,
     allownull: false,
@@ -24,10 +24,10 @@ const start = async () => {
   try {
     console.log("hello world");
     await sequelize.sync({ force: true });
-    await bookmark.create({ website: "google.com", category: "search" });
-    await bookmark.create({ website: "linkedIn.com", category: "jobs" });
-    await bookmark.create({ website: "bing.com", category: "search" });
-    await bookmark.create({ website: "indeed.com", category: "jobs" });
+    await Bookmark.create({ website: "google.com", category: "search" });
+    await Bookmark.create({ website: "linkedIn.com", category: "jobs" });
+    await Bookmark.create({ website: "bing.com", category: "search" });
+    await Bookmark.create({ website: "indeed.com", category: "jobs" });
     const port = process.env.PORT || 3001;
     app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (ex) {
@@ -37,11 +37,69 @@ const start = async () => {
 
 start();
 
-app.get("/", (req, res) => {
-  res.send(`<html>
-        <h1>
-            hello world!
-            
-        </h1>
-    </html>`);
+app.get("/", (req, res, next) => res.redirect("/bookmarks"));
+
+app.get("/bookmarks", async (req, res, next) => {
+  try {
+    const bookmarks = await Bookmark.findAll();
+    const html = bookmarks
+      .map((bookmark) => {
+        return `<div>${bookmark.website}<a href="/categories/${bookmark.category}"> ${bookmark.category}</a></div>`;
+      })
+      .join("");
+    res.send(`
+    <html>
+      <style>
+        div{
+            padding:2rem;
+        }
+      </style>
+      <head>
+        <title> Robby's Bookmark Page</title>
+        <h1>Robby's Bookmark page</h1>
+        <ul>
+        ${html}
+        <ul>
+      </head>
+    <html>
+    `);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/categories/:category", async (req, res, next) => {
+  try {
+    const category = req.params.category;
+    const bookmarks = await Bookmark.findAll({
+      where: { category },
+    });
+    const html = bookmarks
+      .map((bookmark) => {
+        return `<div>${bookmark.website} - ${bookmark.category}</div>`;
+      })
+      .join("");
+    res.send(`
+      <html>
+        <style>
+          div{
+              padding:1 rem;
+          }
+          a{
+              margin:20px;
+          }
+        </style>
+        <head>
+          <title> Robby's Bookmark Page</title>
+          <h1>Robby's Bookmark Page For ${category}</h1>
+          <a href = '/'> << back << </a>
+          <ul>
+          ${html}
+          <ul>
+        </head>
+      <html>
+      `);
+  } catch (ex) {
+    next(ex);
+  }
 });
